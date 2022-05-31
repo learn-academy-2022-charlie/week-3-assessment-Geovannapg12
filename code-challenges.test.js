cbd61adea0c7cfbbaee6ff2935756686718f34fe
Geovannapg12 <geo.p2334@gmail.com>
2022-05-30 22:18:39 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -11,23 +11,65 @@
 // Run the file with the following command: $ yarn jest
 
 
-// --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
+// --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence
 
 // a) Create a test with expect statements for each of the variables provided.
 
+describe ("number", ()=>{
+    it ("returns an array that length containing the numbers of the Fibonacci sequence.", () => {
+    const fibLength1 = 6
+    const fibLength2 = 10
+    expect(number(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+    expect(number(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+    })
+})
+
+// //I got red -number is not defined-
+// //_________________________________________
+// // const fibLength1 = 6
+// // // Expected output: [1, 1, 2, 3, 5, 8]
+// // const fibLength2 = 10
+// // // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+
+// // b) Create the function that makes the test pass.
+//create a function called number
+// use a foor loop that will iterate through the array
+// use .push to add a new value at the end of the array
+
+//for some reason I wanted to use .map but then I realize that would not be of help at all for this problem
+ 
 const fibLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
-
 const fibLength2 = 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+ const number = (num) => {
+     let newArr = [1, 1]
+     for (let i = 2; i < num; i++) {
+        newArr.push(newArr[i-2] + newArr[i - 1])
+     }
+     return newArr
+ }
 
 
-// b) Create the function that makes the test pass.
+// with a lot of help it passed
 
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("arrai", () => {
+    it("returns a new array of only odd numbers sorted from least to greatest.", () => {
+    const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+    const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+    expect(arrai(fullArr1)).toEqual([-9, 7, 9, 199])
+    expect(arrai(fullArr2)).toEqual([-823, 7, 23])
+    })
+})
+
+// //I got red -array is not defined-
+
+// // b) Create the function that makes the test pass.
 
 const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 // Expected output: [-9, 7, 9, 199]
@@ -35,22 +77,74 @@ const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 // Expected output: [-823, 7, 23]
 
+//create a function called array
+// function will return the numbers only the numbers in the array given, and in order from leat to greatest
+//Use .filter to go through the array and find the values that we need
+//Apply the modulo to get numbers that are odd
+// use typeof to make sure we strictly get numbers
+// find way to get the expected output
+
+const arrai = (arr) =>{
+    arr.sort(function(a, b){return a - b})
+    return arr.filter(value => value % 2 !==0 && typeof value === 'number')
+   
+   }
+
+// to sort the numbers from smallesst to largest I tried using Math.min(), I spent a while trying to get it to work but it does not 
+// the other option was .sort, I tried using it inside of the return which did not work. 
+
+
+// // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
+
+// // a) Create a test with expect statements for each of the variables provided.
+
+describe("array1", () => {
+    it("returns an array of the accumulating sum. An empty array should return an empty array.", () => {
+        const numbersToAdd1 = [2, 4, 45, 9]
+        const numbersToAdd2 = [0, 7, -8, 12]
+        const numbersToAdd3 = []
+    expect(array1(numbersToAdd1)).toEqual([2, 6, 51, 60])
+    expect(array1(numbersToAdd2)).toEqual([0, 7, -1, 11])
+    expect(array1(numbersToAdd3)).toEqual([])
+    })
+})
+
+// I got red -array1 is not defined-
+
+// const numbersToAdd1 = [2, 4, 45, 9]
+// // Excpected output: [2, 6, 51, 60]
+
+// const numbersToAdd2 = [0, 7, -8, 12]
+// // Expected output: [0, 7, -1, 11]
+
+// const numbersToAdd3 = []
+// // Expected output: []
+
 
 // b) Create the function that makes the test pass.
+//create a function called array1
+//create an empty array to equal zero
+//use .map that will iterate thorugh the array
+// create conditional  statements 
+
+const array1 = (array) => {
+    let newArray = [0]
+    return array.map((value, index) => {
+        if (index > 0) {
+            return newArray = value + newArray
+        } else {
+            return newArray = value
+         }
+    })
+}
+
+// const array1 = (array) => {
+//     let sum = 0
+//     for (let i=0; i <array1.length; i++){
+//         return sum + array1[i]
+//     }
+// }
 
 
-// --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
-
-// a) Create a test with expect statements for each of the variables provided.
-
-const numbersToAdd1 = [2, 4, 45, 9]
-// Excpected output: [2, 6, 51, 60]
-
-const numbersToAdd2 = [0, 7, -8, 12]
-// Expected output: [0, 7, -1, 11]
-
-const numbersToAdd3 = []
-// Expected output: []
-
-
-// b) Create the function that makes the test pass.
+//while googling how to add the numbers in an array I came across .reduce but it does not work on empty arrays
+// It did not want to work at first and I decided to go with a different way that didn't work either, after getting a second look and help from a different output I noticed that in my test I had not changed the expected output.
--- interview-questions.md
+++ interview-questions.md
@@ -9,59 +9,61 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is `this` in JavaScript?
 
-  Your answer:
+  Your answer: "this" is used as a way to call on an object. I've used this in classes, inheritance and in React. For example when we want to call on a certain object we do this.nameOfObject
 
-  Researched answer:
+  Researched answer: "this" is a javascript keyword that refers to an object.
 
 
 
 2. What is React? Why would you use it?
 
-  Your answer:
+  Your answer: React was created by Facebook and is used to build webpages or applications. react is component based, that means it is used to build small parts and join them all together to build an app or webpage. React is very efficient in the way that it is not repetive, it can be used again and it helps the code look less crowded. I have used in a project that it was meant to traslate words from english to pig latin. I would definietly use it again to make a web page, specially top build a portfolio
 
-  Researched answer:
+  Researched answer: React is used to buil webpages, mobile web pages, and native movile apps. It's component based that means we use it to build many small parts and then join them together to form entire applications.
 
 
 
 3. Which lifecycle method is required in a React class component?
 
-  Your answer:
-
-  Researched answer:
+  Your answer: render is required in a React class component and it renders, gives or provides the JSX code. There is also return which returns what we give inside of the parentheses.
+  Researched answer: Render only works in smart class components, it renders the JSX to display the code that goes within our epmty div. In React, when working with smart components, we need the JSX to live inside a single tag, which goes inside the return(), which lives inside the render(){}.
 
 
 
 4. What is JSX? What is one syntax difference between HTML and JSX?
 
-  Your answer:
+  Your answer: JSX I belive was created by Facebook as well and its a combination between Javascript and HTML. While looking at the difference between and HTML and JSX I've noticed that jsx is more complex but more efficent. HTML is easier to undestand and play around woith but I believe it takes twice the time and effort to write and transfer all the JS files.  
 
-  Researched answer:
+  Researched answer: JSX is JavaScript flavored HTML that behaves almost like HTML. JSX uses the syntax className rather than HTML's class because class is reserved for HTML. 
 
 
 
 5. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-  Your answer:
+  Your answer: Yarn its a package manager. I've used it a couple of times while working on react. What it does is that it downloads files so it can run your code. 
 
-  Researched answer:
+  Researched answer: yarn is a package manager that allows you to run code and share it with other developers.
 
 
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+  Your answer: I believe an anonymous function has the purpose to define something that is not defined yet. 
 
-  Researched answer:
+  Researched answer: anonymous functions allow the creation of functions which have no specific name.
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Conditional rendering:
+1. Conditional rendering: Conditional rendering is a term to describe the ability to render different user interface (UI) markup if a condition is true or false.
 
-2. Object-oriented programming:
+2. Object-oriented programming: OOP focuses on objects rather than functions and logic. "Object-oriented programming (OOP) is a computer programming model that organizes software design around data, or objects, rather than functions and logic. An object can be defined as a data field that has unique attributes and behavior"
 
-3. Ruby:
+3. Ruby: Ruby is object orineted and supposedly simple. "Ruby is a general-purpose, interpreted, dynamically-typed, server-side scripting language. Ruby has many helpful built in methods and great documentation."
 
-4. Ruby blocks:
 
-5. Ruby hashes:
+4. Ruby blocks: Ruby has anonymous functions similar to JavaScript. They are called blocks. Blocks can be created with the do/end keywords OR with {}. Blocks are code that is passed to a method.
+
+5. Ruby hashes: A hash is a container for data. The data in a hash are stored in key:value pairs.
+
+
